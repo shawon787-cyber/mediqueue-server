55ed57b20ab08c0e7cdf928a3dc094fd359c967c
Md Shawon <shawonmohammad787@gmail.com>
2026-05-24 21:47:49 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://mediquee:uqnwokpndaNdjynN@cluster0.73klbbh.mongodb.net/?appName=Cluster0";
 const app = express();
 const PORT = 5000;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
@@ -9,3 +33,7 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+
+// mediquee
+// uqnwokpndaNdjynN
